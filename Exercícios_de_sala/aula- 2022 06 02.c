a46a7c060adca31812c6884e869e930a178a2403
Sombradodia <106778941+Sombradodia@users.noreply.github.com>
2022-06-02 19:50:57 -0300

    Update and rename aula- 2022 06 02.c to Exercícios_de_sala/aula- 2022 06 02.c

---
+++ Exercícios_de_sala/aula- 2022 06 02.c
@@ -0,0 +1,16 @@
+/*Matricula: 2020103117
+inicio:
+/*
+#include <stdio.h>
+int () {
+
+float seg, terc, quar, quint, sex, pag, soma;
+printf("digite o valor estabelecido no dia da entrada")
+scanf("%f seg")
+printf("digite a entrada da seg")
+scanf("%f terc")
+
+
+
+
+}
